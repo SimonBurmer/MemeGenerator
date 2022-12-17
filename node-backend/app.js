9d9340bd99acdfcfcc514f5d92cda033afd38054
Florian Burger <florianburger97@outlook.de>
2022-12-17 18:03:25 +0100

    User Auth

--- node-backend/app.js
+++ node-backend/app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+var cors = require("cors");
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -8,15 +9,70 @@ var logger = require('morgan');
 // ### Your backend project has to switch the MongoDB port like this
 // ### Thus copy paste this block to your project
 const MONGODB_PORT = process.env.DBPORT || '27017';
-const db = require('monk')(`127.0.0.1:${MONGODB_PORT}/omm-2223`); // connect to database omm-2021
-console.log(`Connected to MongoDB at port ${MONGODB_PORT}`)
+// const db = require('monk')(`127.0.0.1:${MONGODB_PORT}/omm-2223`); // connect to database omm-2021
+// console.log(`Connected to MongoDB at port ${MONGODB_PORT}`)
 // ######
+const dbConfig = require("./config/db.config");
+const db = require("./models");
+const Role = db.role;
+const dbUrl = `mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`
+console.log(dbUrl)
+db.mongoose
+  .connect(dbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Successfully connect to MongoDB.");
+    initial();
+  })
+  .catch(err => {
+    console.error("Connection error", err);
+    process.exit();
+  });
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var app = express();
 
+function initial() {
+  Role.estimatedDocumentCount((err, count) => {
+    if (!err && count === 0) {
+      new Role({
+        name: "user"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'user' to roles collection");
+      });
+
+      new Role({
+        name: "moderator"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'moderator' to roles collection");
+      });
+
+      new Role({
+        name: "admin"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'admin' to roles collection");
+      });
+    }
+  });
+}
+
+var app = express();
+app.use(cors({origin: "http://localhost:8081"}))
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -25,11 +81,16 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+require('./routes/auth.routes')(app);
+require('./routes/user.routes')(app);
 
-app.use(function(req,res,next){  req.db = db;
-  next();
+// app.use(function(req,res,next){  req.db = db;
+//   next();
+// });
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
 });
-
 
 // the login middleware. Requires BasicAuth authentication
 app.use((req,res,next) => {
@@ -70,6 +131,12 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
 });
 
 module.exports = app;
---
+++ node-backend/config/db.config.js
@@ -0,0 +1,8 @@
+const MONGODB_PORT = process.env.DBPORT || '27017';
+const DB_PORT ='27017';
+
+module.exports = {
+    HOST: "127.0.0.1",
+    PORT: DB_PORT,
+    DB: "omm-2223"
+  };
