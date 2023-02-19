5062970b7165f731896aa576e164c6cb56f8eb0b
Florian Burger <florianburger97@outlook.de>
2023-02-19 16:01:23 +0100

    Use Effect Fix

--- react-frontend/src/components/editor/tabs/modals/AddTemplates.js
+++ react-frontend/src/components/editor/tabs/modals/AddTemplates.js
@@ -1,10 +1,10 @@
-import Card from 'react-bootstrap/Card';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Container from 'react-bootstrap/Container';
+import Card from "react-bootstrap/Card";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
 import React from "react";
-import { Form } from 'react-bootstrap';
-import InputGroup from 'react-bootstrap/InputGroup';
+import { Form } from "react-bootstrap";
+import InputGroup from "react-bootstrap/InputGroup";
 import { useEffect, useCallback, useState } from "react";
 import TemplateService from "../../../../services/templateService";
 
@@ -15,36 +15,28 @@ function AddTemplates(props) {
 
   useEffect(() => {
     const fetchDataFromAPI = async () => {
+      let p = [];
       const allMemes = await templateService.getAllTemplates();
-      setPreviews(allMemes);
-      console.log(allMemes.length);
+      for (let i = 0; i < allMemes.length; ++i) {
+        p.push(allMemes[i].url);
+      }
+      setPreviews(p);
     };
     fetchDataFromAPI();
   }, []);
 
-  // useEffect(() => {
-  //   if (previews.length > 0) {
-  //     let p = [];
-  //     for (let i = 0; i < previews.length; ++i) {
-  //       p.push(previews[i].url);
-  //     }
-  //     setPreviews(p);
-  //   }
-  // }, [prev]);
-
   return (
     <Container>
-      <Form >
+      <Form>
         <Form.Group required className="mb-3">
           <InputGroup>
             <InputGroup.Text id="btnGroupAddon2">Show</InputGroup.Text>
             <Form.Select
-              onChange={e => 
-                {
-                  setSelected(-1);
-                }}
+              onChange={(e) => {
+                setSelected(-1);
+              }}
             >
-              <option value="User" >Your Templates</option>
+              <option value="User">Your Templates</option>
               <option value="Public">Public Templates</option>
             </Form.Select>
           </InputGroup>
@@ -52,22 +44,26 @@ function AddTemplates(props) {
       </Form>
 
       <Row xs={1} md={2} className="g-4">
-         {
-            previews.map((element, index) => {
-              return <Col key={index}>
-                <Card className={selected === index ? "template-card active" : "template-card"} onClick={() => 
-                  {
-                    props.setImage(element);
-                    setSelected(index);
-                  }}>
-                  <Card.Img variant="top" src={element} />
-                  <Card.Body>
-                    <Card.Title>Template 1</Card.Title>
-                  </Card.Body>
-                </Card>
-              </Col>
-            })
-         }
+        {previews.map((element, index) => {
+          return (
+            <Col key={index}>
+              <Card
+                className={
+                  selected === index ? "template-card active" : "template-card"
+                }
+                onClick={() => {
+                  props.setImage(element);
+                  setSelected(index);
+                }}
+              >
+                <Card.Img variant="top" src={element} />
+                <Card.Body>
+                  <Card.Title>Template 1</Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
+          );
+        })}
       </Row>
     </Container>
   );
