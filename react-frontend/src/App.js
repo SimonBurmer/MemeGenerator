b3677b75ec196cf03ad293bfe2b7fab05747fe2d
Christian Bernhard <44226023+ChristianBernhard@users.noreply.github.com>
2023-02-23 13:58:36 +0100

    [21] Offline Capabilities (PWA) (#12)
    * added likes/dislike plot
    
    * fixed update comments when changing singleview
    
    * Basic PWA Offline Functionality

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -3,13 +3,14 @@ import Home from "./components/home/Home";
 import Editor from "./components/editor/Editor";
 import Overview from "./components/overview/Overview";
 import Login from "./components/login/Login";
-import { useEffect, useState } from "react";
+import React from 'react';
+import {useEffect, useState} from "react";
 
 import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate,
+    BrowserRouter as Router,
+    Route,
+    Routes,
+    Navigate,
 } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -17,29 +18,50 @@ import "./App.css";
 import Profile from "./components/profile/Profile";
 
 function App() {
-  const [user, setUser] = useState(null);
-  const [authState, setAuthState] = useState(false);
+    const [user, setUser] = useState(null);
+    const [authState, setAuthState] = useState(false);
 
-  return (
-    <Router>
-      <div className="App">
-        <TopNavigationBar userName={user} />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route
-            exact
-            path="/login"
-            element={user ? <Navigate to="/" /> : <Login />}
-          />
-          <Route path="/home" element={<Home />} />
-          <Route path="/editor" element={<Editor />} />
-          <Route path="/overview" element={<Overview isUserProfile={false}/>} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-      </div>
-    </Router>
-  );
+    const [isOnline, setIsOnline] = React.useState(navigator.onLine);
+
+    React.useEffect(() => {
+        window.addEventListener('offline', () => setIsOnline(false));
+        window.addEventListener('online', () => setIsOnline(true));
+
+        return () => {
+            window.removeEventListener('offline', () => setIsOnline(false));
+            window.removeEventListener('online', () => setIsOnline(true));
+        }
+    }, []);
+
+    return (
+        <div>
+            {isOnline ? (
+                <Router>
+                    <div className="App">
+                        <TopNavigationBar userName={user}/>
+                        <Routes>
+                            <Route path="/" element={<Home/>}/>
+                            <Route
+                                exact
+                                path="/login"
+                                element={user ? <Navigate to="/"/> : <Login/>}
+                            />
+                            <Route path="/home" element={<Home/>}/>
+                            <Route path="/editor" element={<Editor/>}/>
+                            <Route path="/overview" element={<Overview isUserProfile={false}/>}/>
+                            <Route path="/profile" element={<Profile/>}/>
+                            <Route path="*" element={<Navigate to="/"/>}/>
+                        </Routes>
+                    </div>
+                </Router>
+            ) : (
+                <>
+                    <h2>Without internet connection neither contents can be viewed nor edited...</h2>
+                    <h4>Please make sure to have a stable internet connection</h4>
+                </>
+            )}
+        </div>
+    );
 }
 
 export default App;
